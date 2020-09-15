1ddcc91cc2c5cf5b81517db0605492a4c8337b5c
Jamison J. <39608261+jbjohnson1278@users.noreply.github.com>
2020-09-15 19:53:55 -0400

    made one paddle and ball

--- src/javapong/Tennis.java
+++ src/javapong/Tennis.java
@@ -3,35 +3,85 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package javapong;
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-/**
- *
- * @author Owner
- */
-public class Tennis extends Applet {
-
-    /**
-     * Initialization method that will be called after the applet is loaded into
-     * the browser.
-     */
+public class Tennis extends Applet implements Runnable, KeyListener {
     final int WIDTH = 700, HEIGHT = 500;
+    Thread thread;
+    HumanPaddle p1;
+    Ball b1;
+    
     public void init() {
-        // TODO start asynchronous download of heavy resources
         this.resize(WIDTH, HEIGHT);
+        this.addKeyListener(this);
+        p1 = new HumanPaddle(1);
+        b1 = new Ball();
+        thread = new Thread(this);
+        thread.start();
     }
     
     public void paint(Graphics g) {
         g.setColor(Color.black);
         g.fillRect(0, 0, WIDTH, HEIGHT);
+        if(b1.getX() < -10 || b1.getX() > 710) {
+            g.setColor(Color.red);
+            g.drawString("Game Over", 350, 250);
+        } else {
+            p1.draw(g);
+            b1.draw(g);
+        }
     }
     
     public void update(Graphics g) {
         paint(g);
     }
+    
+    public void run() { //keeps the game running
+        while(true) {
+            p1.move();
+            b1.move();
+            b1.checkPaddleCollision(p1, p1);
+            repaint();
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {    
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    
 
     // TODO overwrite start(), stop() and destroy() methods
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_UP) {
+            p1.setUpAccel(true);
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            p1.setDownAccel(true);
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_UP) {
+            p1.setUpAccel(false);
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            p1.setDownAccel(false);
+        }
+    }
 }
